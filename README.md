bda6f1a77ff3c8fca394d5553c8645ddec6a6228
Jacky Yang <jackyyang1670@gmail.com>
2024-08-04 23:17:27 -0700

    Project finished
    Co-authored-by: jordles <jackyyang1670@gmail.com>
    Co-authored-by: aizazhalima1 <halimaaizaz@hotmail.com>

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 ## Grocery List (OOP)  
 
-This assignment focused on OOP practices. We made a grocery list that you can add items to and edit dynamically. 
+This assignment focused on OOP practices. We made a grocery list that you can add items to and edit dynamically. Items are saved using Local Storage. 
 
+# How its Made
+
+Tech Used: HTML, CSS, Javascript
+
+# Lessons Learned
+
+We learned a lot about Local Storage. We didn't realize that it was parsed as a string in values. So we utilized JSON.parse (converts the strings back to our datatype) and JSON.stringify (converting our other datatypes to strings). We also used new event listeners like blur() when interacting with the focused state. What was challenging was implementing the proper logic for editing our list items live, but contenteditable property came in handy. 
 
 # Attribution 
-
 [background](https://unsplash.com/photos/goods-on-shelf-rWMIbqmOxrY)
--- js/main.js
+++ js/main.js
@@ -1,50 +1,77 @@
 const inputForm = document.querySelector('#item')
-
 class Grocery{
   constructor(){
-
+    this.groceries = JSON.parse(localStorage.getItem('groceries')) || [];
   }
-  addItem(){
-    console.log(inputForm.value)
 
+  updateDOM(){
+    if(!this.groceries) return;
+    this.groceries.forEach(item => this.createListItem(item));
+  }
+
+  updateLocalStorage(){
+    localStorage.setItem('groceries', JSON.stringify(this.groceries)); //stringify converts array to string so we can store it in local storage
+  }
+    
+  createListItem(listText){
     const li = document.createElement('li');
     li.innerHTML = 
-    `<span class="item">${inputForm.value}</span> 
-    <span class="material-symbols-outlined" id="edit"> edit </span> <span class="material-symbols-outlined" id="trash"> delete </span>
+    ` <span class="item">${listText}</span> 
+      <span class="material-symbols-outlined" id="edit"> edit </span> <span class="material-symbols-outlined" id="trash"> delete </span>
     `;
     document.querySelector('ul').appendChild(li);
 
     //add event listener
     li.querySelector('#trash').addEventListener('click', (e) => this.deleteItem(e));
     li.querySelector('#edit').addEventListener('click',(e) => this.editItem(e));
-
   }
+
+  addItem(){
+    this.createListItem(inputForm.value);
+    this.groceries.push(inputForm.value) //add to array of groceries
+    this.updateLocalStorage(); //update the groceries local storage
+    inputForm.value = ''; //clear the input
+  }
+
   editItem(event){
-    event.target.parentNode.querySelector(".item").contentEditable = true; //modify
-    event.target.parentNode.querySelector(".item").focus(); //put our list item on focused state
-    event.target.parentNode.querySelector(".item").addEventListener('keydown',(e)=>{
-     if( e.key === "Enter"){
-      event.target.parentNode.querySelector(".item").contentEditable =false;
-     }
-      
-
+    let currentText = event.target.parentNode.children[0].innerText;
+    let editIndex = this.groceries.indexOf(currentText)
+    const currentItem = event.target.parentNode.querySelector('.item');
+    currentItem.contentEditable = true; //modify
+    currentItem.focus(); //put our list item on focused state
+    currentItem.addEventListener('keydown',(e)=>{
+     if( e.key === "Enter") this.doneEditing(e, editIndex);
     })
+    currentItem.addEventListener('blur',(e)=>{
+      this.doneEditing(e, editIndex);
+    })
+  }
 
+  doneEditing(event, editIndex){
+    event.target.parentNode.querySelector(".item").contentEditable =false;
+    let toEditText = event.target.parentNode.children[0].innerText
+    this.groceries[editIndex] = toEditText; 
+    this.updateLocalStorage();
   }
 
   deleteItem(event){
-    console.log(event.target.parentNode);
-    //remove li element
+    let toDeleteText = event.target.parentNode.children[0].innerText
     document.querySelector('ul').removeChild(event.target.parentNode)
-    //we target the parentnode, we use the method to remove a child from the parent
+    this.groceries.splice(this.groceries.indexOf(toDeleteText),1);
+    this.updateLocalStorage(); 
   }
 
   clearList(){
     document.querySelector("ul").innerHTML = '';
+    this.groceries = [];
+    localStorage.clear();
   }
 }
 
-const groceryList=new Grocery()
+const groceryList = new Grocery()
+
+groceryList.updateDOM();
+
 document.querySelector('#add').addEventListener('click',(event) => {
   event.preventDefault(); //prevent the page from refreshing after submit
   groceryList.addItem()
@@ -53,4 +80,5 @@ document.querySelector('#add').addEventListener('click',(event) => {
 document.querySelector('#clear').addEventListener('click',()=>{
   groceryList.clearList()
 })
+
 
